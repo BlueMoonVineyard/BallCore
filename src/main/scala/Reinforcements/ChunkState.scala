2f17c6fece37555958b13bb6c01469d09f5b2ab4
Janet Blackquill <uhhadd@gmail.com>
2022-12-21 22:03:38 -0500

    feat, wip: reinforcements

---
+++ src/main/scala/Reinforcements/ChunkState.scala
@@ -0,0 +1,39 @@
+// SPDX-FileCopyrightText: 2022 Janet Blackquill <uhhadd@gmail.com>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+package BallCore.Reinforcements
+
+import java.{util => ju}
+import BallCore.Groups
+import scala.collection.mutable.Map
+import io.circe._, io.circe.generic.semiauto._
+
+/** This keys the reinforcement state for a given block in a chunkstate */
+case class BlockKey(
+    offsetX: Int,
+    offsetZ: Int,
+    y: Int,
+)
+
+/** This holds the reinforcement state for a given block in a chunkstate */
+case class BlockState(
+    group: Groups.GroupID,
+    dirty: Boolean,
+    deleted: Boolean,
+)
+
+/** This keys ChunkStates in the cache */
+case class ChunkKey(
+    chunkX: Int,
+    chunkZ: Int,
+    world: String,
+)
+
+/** This holds reinforcement information for a single chunk (the Map is mutable for performance reasons) */
+case class ChunkState(
+    // chunkX: Int,
+    // chunkZ: Int,
+    // world: ju.UUID,
+    blocks: Map[BlockKey, BlockState],
+)
---
+++ src/main/scala/Reinforcements/ChunkStateManager.scala
@@ -0,0 +1,109 @@
+// SPDX-FileCopyrightText: 2022 Janet Blackquill <uhhadd@gmail.com>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+package BallCore.Reinforcements
+
+import BallCore.Storage
+import BallCore.DataStructures.LRUCache
+
+import scala.collection.mutable.Map
+
+import scalikejdbc._
+import scalikejdbc.SQL
+import scalikejdbc.NoExtractor
+
+/** The ChunkStateManager is responsible for managing the loading and saving of chunkstates to a SQL database */
+class ChunkStateManager()(using sql: Storage.SQLManager):
+    sql.applyMigration(
+        Storage.Migration(
+            "Initial ChunkStateManager",
+            List(
+                sql"""
+                CREATE TABLE Reinforcements (
+                    ChunkX INTEGER NOT NULL,
+                    ChunkZ INTEGER NOT NULL,
+                    World TEXT NOT NULL,
+                    OffsetX INTEGER NOT NULL,
+                    OffsetZ INTEGER NOT NULL,
+                    Y INTEGER NOT NULL,
+                    Group TEXT NOT NULL,
+                );
+                """
+            ),
+            List(
+                sql"""
+                DROP TABLE Reinforcements;
+                """
+            ),
+        )
+    )
+
+    private implicit val session: DBSession = AutoSession
+    private val cache = LRUCache[ChunkKey, ChunkState](1000, evict)
+
+    private def evict(key: ChunkKey, value: ChunkState): Unit =
+        set(key, value)
+
+    private def load(key: ChunkKey): Unit =
+        val cs = ChunkState(Map())
+        val items =
+            sql"""
+            SELECT (OffsetX, OffsetZ, Y, Group) FROM ChunkStateManager
+                WHERE ChunkX = ${key.chunkX}
+                  AND ChunkZ = ${key.chunkZ}
+                  AND World = ${key.world};
+            """
+                .map(rs => (rs.int("OffsetX"), rs.int("OffsetZ"), rs.int("Y"), rs.string("Group")))
+                .list
+                .apply()
+                .foreach { tuple =>
+                    val (offsetX, offsetZ, y, group) = tuple
+                    cs.blocks(BlockKey(offsetX, offsetZ, y)) = BlockState(???, false, false)
+                }
+        cache(key) = cs
+
+    def get(key: ChunkKey): ChunkState =
+        if !cache.contains(key) then
+            load(key)
+        if !cache.contains(key) then
+            cache(key) = ChunkState(Map())
+        cache(key)
+        
+
+    def set(key: ChunkKey, value: ChunkState): Unit =
+        val cx = key.chunkX
+        val cz = key.chunkZ
+        val cw = key.world
+
+        DB.localTx { implicit session =>
+            value.blocks.view.filter(_._2.deleted).foreach { item =>
+                val (key, _) = item
+                sql"""
+                DELETE FROM Reinforcements
+                    WHERE ChunkX = ${cx}
+                      AND ChunkZ = ${cz}
+                      AND World = ${cw}
+                      AND OffsetX = ${key.offsetX}
+                      AND OffsetZ = ${key.offsetZ}
+                      AND Y = ${key.y};
+                """.update.apply()
+            }
+            value.blocks.view.filter(item => item._2.dirty && !item._2.deleted).foreach { item =>
+                val (key, value) = item
+                sql"""
+                INSERT OR REPLACE INTO Reinforcements
+                    (ChunkX, ChunkZ, World, OffsetX, OffsetZ, Y, Group)
+                    VALUES
+                    (${cx},  ${cz},  ${cw}, ${key.offsetX}, ${key.offsetZ}, ${key.y}, ${value.group})
+                """
+            }
+        }
+
+        value.blocks.filterInPlace( (key, value) => !value.deleted )
+        value.blocks.mapValuesInPlace ( (key, value) =>
+            if value.dirty then
+                value.copy(dirty = false)
+            else
+                value
+        )
