af5c9e3b2d60c2176f715d3b3c251f3105e66e55
Janet Blackquill <uhhadd@gmail.com>
2023-03-13 22:37:34 -0400

    feat, wip: multiblocks

---
+++ src/main/scala/Multiblocks/MultiblockManager.scala
@@ -0,0 +1,43 @@
+package BallCore.Multiblocks
+
+import BallCore.Storage
+import scalikejdbc._
+import scalikejdbc.SQL
+import scalikejdbc.NoExtractor
+
+class MultiblockManager(using sql: Storage.SQLManager):
+    sql.applyMigration(
+        Storage.Migration(
+            "Initial MultiblockManager",
+            List(
+                sql"""
+                CREATE TABLE BlocksToMultiblocks (
+                    BlockX INTEGER NOT NULL,
+                    BlockY INTEGER NOT NULL,
+                    BlockZ INTEGER NOT NULL,
+                    World TEXT NOT NULL,
+                    Multiblock TEXT NOT NULL,
+                    FOREIGN KEY (Multiblock) REFERENCES Multiblocks(ID) ON DELETE CASCADE
+                );
+                """,
+                sql"""
+                CREATE TABLE Multiblocks (
+                    ID TEXT NOT NULL,
+                    Class TEXT NOT NULL,
+                    Data BLOB NOT NULL,
+                    UNIQUE(ID)
+                );
+                """,
+            ),
+            List(
+                sql"""
+                DROP TABLE BlocksToMultiblocks;
+                """,
+                sql"""
+                DROP TABLE Multiblocks;
+                """,
+            ),
+        )
+    )
+
+    private implicit val session: DBSession = AutoSession
