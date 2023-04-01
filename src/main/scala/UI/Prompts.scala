05cb0fca1c3f9dcb374da4fe10bccf71ef161464
Janet Blackquill <uhhadd@gmail.com>
2023-03-31 21:16:40 -0400

    refactor: port away from conversations

--- src/main/scala/UI/Prompts.scala
+++ src/main/scala/UI/Prompts.scala
@@ -8,52 +8,49 @@ import java.util.UUID
 import scala.concurrent.Promise
 import org.bukkit.event.Listener
 import org.bukkit.plugin.Plugin
-import org.bukkit.conversations.Conversation
 import org.bukkit.entity.Player
-import org.bukkit.conversations.ConversationFactory
-import org.bukkit.conversations.StringPrompt
-import org.bukkit.conversations.ConversationContext
-import org.bukkit.conversations.Prompt
 import scala.concurrent.Future
 import scala.util.Try
 import scala.concurrent.ExecutionContext
 import BallCore.Folia.EntityExecutionContext
+import org.bukkit.event.EventHandler
+import io.papermc.paper.event.player.AsyncChatEvent
+import org.bukkit.event.EventPriority
+import net.kyori.adventure.text.serializer.plain.PlainTextComponentSerializer
+import net.kyori.adventure.text.Component
 
 private case class PromptState(
     promise: Promise[String],
-    conversation: Conversation,
 )
 
-class Prompts(using plugin: Plugin):
+class Prompts(using plugin: Plugin) extends Listener:
     private val prompts = scala.collection.concurrent.TrieMap[UUID, PromptState]()
+    plugin.getServer().getPluginManager().registerEvents(this, plugin)
+
+    private def stringify(c: Component) = PlainTextComponentSerializer.plainText().serialize(c)
+
+    @EventHandler(priority = EventPriority.NORMAL, ignoreCancelled = true)
+    def playerChat(event: AsyncChatEvent): Unit =
+        prompts.get(event.getPlayer().getUniqueId()).map { prompt =>
+            event.setCancelled(true)
+            prompt.promise.complete(Try(stringify(event.message())))
+            prompts.remove(event.getPlayer().getUniqueId())
+        }
 
     def prompt(player: Player, prompt: String): Future[String] =
         if prompts.contains(player.getUniqueId()) then
             val state = prompts(player.getUniqueId())
             state.promise.failure(Exception("cancelled by another prompt"))
-            state.conversation.abandon()
             prompts.remove(player.getUniqueId())
 
         val promise = Promise[String]()
         given ctx: ExecutionContext = EntityExecutionContext(player)
 
-        val bukkitPrompt = new StringPrompt:
-            override def getPromptText(context: ConversationContext): String =
-                prompt
-            override def acceptInput(context: ConversationContext, input: String): Prompt =
-                prompts(player.getUniqueId()).promise.complete(Try(input))
-                prompts.remove(player.getUniqueId())
-                Prompt.END_OF_CONVERSATION
-
-        val conversation = ConversationFactory(plugin)
-            .withModality(false)
-            .withLocalEcho(false)
-            .withFirstPrompt(bukkitPrompt)
-            .buildConversation(player)
-            
-        val state = PromptState(promise, conversation)
-        Future { player.closeInventory() }
-        conversation.begin()
+        val state = PromptState(promise)
+        Future {
+            player.closeInventory()
+            player.sendMessage(prompt)
+        }
         prompts(player.getUniqueId()) = state
         promise.future
 
