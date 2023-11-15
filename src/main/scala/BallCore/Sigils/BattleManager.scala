0706c328868b6f73bee4710138919efde5dca2ab
Janet Blackquill <uhhadd@gmail.com>
2023-11-14 23:11:38 -0500

    more battling

--- src/main/scala/BallCore/Sigils/BattleManager.scala
+++ src/main/scala/BallCore/Sigils/BattleManager.scala
@@ -2,13 +2,13 @@ package BallCore.Sigils
 
 import BallCore.Storage
 import skunk.implicits._
-import BallCore.Groups.GroupID
 import java.util.UUID
 import skunk.Session
 import cats.effect.IO
 import skunk.codec.all._
 import skunk.SqlState
 import cats.syntax.traverse.*
+import BallCore.Beacons.BeaconID
 
 type BattleID = UUID
 
@@ -25,40 +25,51 @@ class BattleManager(using sql: Storage.SQLManager, hooks: BattleHooks):
             "Initial Battle Manager",
             List(
                 sql"""
-				CREATE TABLE Battles(
-					BattleID UUID PRIMARY KEY,
-					OffensiveGroup UUID NOT NULL,
-					DefensiveGroup UUID NOT NULL,
-					Health INTEGER NOT NULL,
-					UNIQUE(OffensiveGroup, DefensiveGroup),
+                CREATE TABLE Battles(
+                    BattleID UUID PRIMARY KEY,
+                    OffensiveBeacon UUID NOT NULL,
+                    DefensiveBeacon UUID NOT NULL,
+                    Health INTEGER NOT NULL,
+                    PillarCount INTEGER NOT NULL,
+                    FOREIGN KEY (OffensiveBeacon) REFERENCES CivBeacons (ID),
+                    FOREIGN KEY (DefensiveBeacon) REFERENCES CivBeacons (ID),
+                    UNIQUE(OffensiveBeacon, DefensiveBeacon),
                     CHECK(Health BETWEEN 1 AND 10)
-				);
-				""".command
+                );
+                """.command
             ),
             List(),
         )
     )
 
-    private def spawnInitialPillars(battle: BattleID): IO[Unit] =
-        (1 to 5).toList.traverse(_ => hooks.spawnPillarFor(battle)).map(_ => ())
+    private def spawnInitialPillars(battle: BattleID, count: Int): IO[Unit] =
+        (1 to count).toList.traverse(_ => hooks.spawnPillarFor(battle)).map(_ => ())
 
-    def startBattle(offensive: GroupID, defensive: GroupID)(using
+    def startBattle(offensive: BeaconID, defensive: BeaconID)(using
         Session[IO]
     ): IO[BattleID] =
         sql.useIO(
             sql.queryUniqueIO(
                 sql"""
             INSERT INTO Battles (
-                BattleID, OffensiveGroup, DefensiveGroup, Health
-            ) VALUES (
-                gen_random_uuid(), $uuid, $uuid, $int4
-            ) RETURNING BattleID;
+                BattleID, OffensiveBeacon, DefensiveBeacon, Health, PillarCount
+            ) SELECT
+                gen_random_uuid() as BattleID,
+                $uuid as OffensiveBeacon,
+                $uuid as DefensiveBeacon,
+                $int4 as Health,
+                GREATEST(FLOOR(ST_Area(ST_Intersection(
+                    (SELECT CoveredArea FROM CivBeacons WHERE ID = $uuid),
+                    (SELECT CoveredArea FROM CivBeacons WHERE ID = $uuid)
+                )) / 512.0), 1)  as PillarCount
+            RETURNING BattleID, PillarCount;
             """,
-                uuid,
-                (offensive, defensive, initialHealth),
-            )).flatTap { battleID =>
-                spawnInitialPillars(battleID)
-            }
+                (uuid *: int4),
+                (offensive, defensive, initialHealth, offensive, defensive),
+            )
+        ).flatTap { (battleID, count) =>
+            spawnInitialPillars(battleID, count)
+        }.map(_._1)
     def pillarDefended(battle: BattleID)(using Session[IO]): IO[Unit] =
         sql.useIO(
             sql.commandIO(
