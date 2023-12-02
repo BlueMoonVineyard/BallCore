3051a192e0b00e114acf6b5e544f5629f10afdbf
Janet Blackquill <uhhadd@gmail.com>
2023-12-02 12:59:43 -0500

    fix: adaptation is in a world of 5k, not 2k

--- src/main/scala/BallCore/Acclimation/Information.scala
+++ src/main/scala/BallCore/Acclimation/Information.scala
@@ -7,7 +7,7 @@ package BallCore.Acclimation
 import org.bukkit.Bukkit
 
 object Information:
-    private val WorldRadiusBlocks = 2000.0
+    private val WorldRadiusBlocks = 5000.0
     private val ExclusionZoneSize = 200.0
     private val SeaLevel = 67
 
