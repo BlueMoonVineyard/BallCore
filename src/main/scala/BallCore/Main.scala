627ad3827c3af0d09cde42bfef67dd4c00fabc41
Janet Blackquill <uhhadd@gmail.com>
2023-12-03 15:31:21 -0500

    feat: make sure server doesn't start up if ballcore isn't working

--- src/main/scala/BallCore/Main.scala
+++ src/main/scala/BallCore/Main.scala
@@ -52,141 +52,146 @@ final class Main extends JavaPlugin:
     given sm: ShutdownCallbacks = ShutdownCallbacks()
 
     override def onEnable(): Unit =
-        CommandAPI.onLoad(CommandAPIBukkitConfig(this))
-        CommandAPI.onEnable()
+        try
+            CommandAPI.onLoad(CommandAPIBukkitConfig(this))
+            CommandAPI.onEnable()
 
-        val dataDirectory = getDataFolder.toPath
-        val loader = YamlConfigurationLoader
-            .builder()
-            .path(dataDirectory.resolve("config.yaml"))
-            .build()
-        Files.createDirectories(dataDirectory)
-        val config = Try(loader.load()).get
-        val databaseConfig = Config.from(config.node("database")) match
-            case Left(err) =>
-                throw Exception(s"failed to read config because $err")
-            case Right(value) =>
-                value
+            val dataDirectory = getDataFolder.toPath
+            val loader = YamlConfigurationLoader
+                .builder()
+                .path(dataDirectory.resolve("config.yaml"))
+                .build()
+            Files.createDirectories(dataDirectory)
+            val config = Try(loader.load()).get
+            val databaseConfig = Config.from(config.node("database")) match
+                case Left(err) =>
+                    throw Exception(s"failed to read config because $err")
+                case Right(value) =>
+                    value
 
-        given sql: Storage.SQLManager =
-            sm.addIO(Storage.SQLManager(databaseConfig))
+            given sql: Storage.SQLManager =
+                sm.addIO(Storage.SQLManager(databaseConfig))
 
-        given keyVal: Storage.SQLKeyVal = new Storage.SQLKeyVal
+            given keyVal: Storage.SQLKeyVal = new Storage.SQLKeyVal
 
-        given acclimation: Acclimation.Storage = new Acclimation.Storage
+            given acclimation: Acclimation.Storage = new Acclimation.Storage
 
-        given ballcore: Main = this
+            given ballcore: Main = this
 
-        given prompts: UI.Prompts = new UI.Prompts
+            given prompts: UI.Prompts = new UI.Prompts
 
-        given gm: GroupManager = new GroupManager
+            given gm: GroupManager = new GroupManager
 
-        given csm: ChunkStateManager = new ChunkStateManager
+            given csm: ChunkStateManager = new ChunkStateManager
 
-        given esm: EntityStateManager = new EntityStateManager
+            given esm: EntityStateManager = new EntityStateManager
 
-        given clock: Clock = new WallClock
+            given clock: Clock = new WallClock
 
-        given hm: HologramManager = new HologramManager
+            given hm: HologramManager = new HologramManager
 
-        given hn: CivBeaconManager = new CivBeaconManager
+            given hn: CivBeaconManager = new CivBeaconManager
 
-        given erm: EntityReinforcementManager = new EntityReinforcementManager
+            given erm: EntityReinforcementManager = new EntityReinforcementManager
 
-        given ac: AntiCheeser = new AntiCheeser
+            given ac: AntiCheeser = new AntiCheeser
 
-        given server: Server = Bukkit.getServer
+            given server: Server = Bukkit.getServer
 
-        given reg: ItemRegistry = BasicItemRegistry()
+            given reg: ItemRegistry = BasicItemRegistry()
 
-        given bm: BlockManager = KeyValBlockManager()
+            given bm: BlockManager = KeyValBlockManager()
 
-        given cem: CustomEntityManager = new CustomEntityManager()
+            given cem: CustomEntityManager = new CustomEntityManager()
 
-        given bam: SigilSlimeManager = SigilSlimeManager()
-        given spm: SlimePillarManager = SlimePillarManager()
-        given ingameBattleHooks: BattleHooks = GameBattleHooks()
-        given battleManager: BattleManager = new BattleManager()
-        given editor: PolygonEditor = new PolygonEditor()
-        given ott: OneTimeTeleporter = OneTimeTeleporter(
-            GameOneTimeTeleporterHooks()
-        )
+            given bam: SigilSlimeManager = SigilSlimeManager()
+            given spm: SlimePillarManager = SlimePillarManager()
+            given ingameBattleHooks: BattleHooks = GameBattleHooks()
+            given battleManager: BattleManager = new BattleManager()
+            given editor: PolygonEditor = new PolygonEditor()
+            given ott: OneTimeTeleporter = OneTimeTeleporter(
+                GameOneTimeTeleporterHooks()
+            )
 
-        given editor3D: PolyhedraEditor = new PolyhedraEditor()
+            given editor3D: PolyhedraEditor = new PolyhedraEditor()
 
-        given lib: ScoreboardLibrary =
-            ScoreboardLibrary.loadScoreboardLibrary(this)
+            given lib: ScoreboardLibrary =
+                ScoreboardLibrary.loadScoreboardLibrary(this)
 
-        given sid: SidebarActor = SidebarActor()
+            given sid: SidebarActor = SidebarActor()
 
-        given busts: BustThroughTracker = BustThroughTracker()
+            given busts: BustThroughTracker = BustThroughTracker()
 
-        given restHooks: RestManagerHooks = IngameRestManagerHooks()
-        given rest: RestManager = RestManager()
-        given fingerprints: FingerprintManager =
-            Fingerprints.Fingerprints.register()
+            given restHooks: RestManagerHooks = IngameRestManagerHooks()
+            given rest: RestManager = RestManager()
+            given fingerprints: FingerprintManager =
+                Fingerprints.Fingerprints.register()
 
-        sid.startListener()
+            sid.startListener()
 
-        Datekeeping.Datekeeping.startSidebarClock()
-        Beacons.registerItems()
-        QuadrantOres.registerItems()
-        QuadrantGear.registerItems()
-        CardinalOres.registerItems()
-        Furnace.registerItems()
-        Reinforcements.register()
-        CustomItemListener.register()
-        given List[CraftingStation] = CraftingStations.register()
-        Rest.Rest.register()
-        SpawnBook.register()
-        SpawnInventory.Listener.register()
-        Alloys.Tier1.register()
-        Tier1Gear.registerItems()
-        InformationGiver().register()
+            Datekeeping.Datekeeping.startSidebarClock()
+            Beacons.registerItems()
+            QuadrantOres.registerItems()
+            QuadrantGear.registerItems()
+            CardinalOres.registerItems()
+            Furnace.registerItems()
+            Reinforcements.register()
+            CustomItemListener.register()
+            given List[CraftingStation] = CraftingStations.register()
+            Rest.Rest.register()
+            SpawnBook.register()
+            SpawnInventory.Listener.register()
+            Alloys.Tier1.register()
+            Tier1Gear.registerItems()
+            InformationGiver().register()
 
-        given aa: AcclimationActor = AcclimationActor.register()
-        given rs: RandomSpawn = RandomSpawn()
+            given aa: AcclimationActor = AcclimationActor.register()
+            given rs: RandomSpawn = RandomSpawn()
 
-        PolyhedraEditor.register()
-        PolygonEditor.register()
-        Mining.Mining.register()
-        RandomSpawner.Listener.register()
-        MapCloningListener.register()
-        Sigil.register()
+            PolyhedraEditor.register()
+            PolygonEditor.register()
+            Mining.Mining.register()
+            RandomSpawner.Listener.register()
+            MapCloningListener.register()
+            Sigil.register()
 
-        given pbm: PlantBatchManager = Plants.Plants.register()
+            given pbm: PlantBatchManager = Plants.Plants.register()
 
-        given chatActor: ChatActor = Chat.Chat.register()
+            given chatActor: ChatActor = Chat.Chat.register()
 
-        val chatCommands = ChatCommands()
-        Order.register()
-        // HTTP.register()
-        chatCommands.group.register()
-        chatCommands.global.register()
-        chatCommands.local.register()
-        val groupCommands = GroupsCommand()
-        groupCommands.node.register()
-        groupCommands.invitesNode.register()
-        BindHeartCommand().node.register()
-        GetHeart().node.register()
-        BookCommand().node.register()
-        CancelCommand().node.register()
-        DeclareCommand().node.register()
-        CheatCommand().node.register()
-        DoneCommand().node.register()
-        PlantsCommand().node.register()
-        OTTCommand().node.register()
-        Messaging.register()
-        GammaCommand().node.register()
-        OneTimeAdaptation().node.register()
-        RestartTimer().register()
-        Elevators.register()
-        val msg = MessageCommand()
-        msg.node.`override`()
-        msg.replyNode.register()
-        msg.meNode.`override`()
-        MyFingerprintCommand().node.register()
-        StationCommand().node.register()
+            val chatCommands = ChatCommands()
+            Order.register()
+            // HTTP.register()
+            chatCommands.group.register()
+            chatCommands.global.register()
+            chatCommands.local.register()
+            val groupCommands = GroupsCommand()
+            groupCommands.node.register()
+            groupCommands.invitesNode.register()
+            BindHeartCommand().node.register()
+            GetHeart().node.register()
+            BookCommand().node.register()
+            CancelCommand().node.register()
+            DeclareCommand().node.register()
+            CheatCommand().node.register()
+            DoneCommand().node.register()
+            PlantsCommand().node.register()
+            OTTCommand().node.register()
+            Messaging.register()
+            GammaCommand().node.register()
+            OneTimeAdaptation().node.register()
+            RestartTimer().register()
+            Elevators.register()
+            val msg = MessageCommand()
+            msg.node.`override`()
+            msg.replyNode.register()
+            msg.meNode.`override`()
+            MyFingerprintCommand().node.register()
+            StationCommand().node.register()
+        catch
+            case _ =>
+                getSLF4JLogger().error("Failed to start BallCore, shutting down...")
+                Bukkit.getServer().shutdown()
 
     override def onDisable(): Unit =
         CommandAPI.onDisable()
